63392d2676fd513b212c4b1ea348f70e253e5092
Janos Mezo <jmezo1992@gmail.com>
2020-02-07 00:17:16 +0100

    added follow button

--- src/app/users/user-header/user-header.component.ts
+++ src/app/users/user-header/user-header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ImageService } from 'src/app/posts/image.service';
 import { UserService } from '../user.service';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-user-header',
@@ -11,9 +12,14 @@ export class UserHeaderComponent implements OnInit {
   @Input() username: string;
   @Input() height: string= "50px";
   imageRef: any = 'https://images-na.ssl-images-amazon.com/images/I/513vjQ3OzFL._AC_SX522_.jpg';
-  constructor(private userService: UserService, private imageService: ImageService) { }
+  @Input() enableFollow: boolean = true;
+  isCurrentUser: boolean;
+  isFollowing: boolean = false;
+
+  constructor(private userService: UserService, private imageService: ImageService, private authservice: AuthService) { }
 
   ngOnInit() {
+    this.isCurrentUser = this.authservice.user.getValue().username == this.username;
     this.userService.checkHasImage(this.username).subscribe(hasImage => {
       if (hasImage) {
         this.imageService.getUserImage(this.username).subscribe( image => {
@@ -23,4 +29,7 @@ export class UserHeaderComponent implements OnInit {
     });
   }
 
+  onClickFollow() {
+    this.isFollowing = !this.isFollowing;
+  }
 }
